584308e69d7f6303d4de2e91b1133896de816638
WataruMaeda <wm19850716@gmail.com>
2016-10-01 16:45:59 +0900

    写真表示部分の実装

--- samplePhotoFrameApp/ViewController.swift
+++ samplePhotoFrameApp/ViewController.swift
@@ -8,11 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet var myScrollView: UIScrollView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.layoutPhotos()
+    }
+    
+    func layoutPhotos()
+    {
+        
+        // 写真の(x,y,幅,高さ)を取得
+        var x = 0 as CGFloat
+        let y = 0 as CGFloat
+        let width = myScrollView.frame.size.width
+        let height = myScrollView.frame.size.height
+        
+        // １枚目の写真
+        let myImageView1 = UIImageView()
+        myImageView1.frame = CGRectMake(x, y, width, height)
+        myImageView1.image = UIImage(named: "photo-1.jpg")
+        myImageView1.contentMode = .ScaleAspectFill
+        myImageView1.clipsToBounds = true
+        myScrollView!.addSubview(myImageView1)
+        
+        // 2枚目の写真
+        x += width
+        let myImageView2 = UIImageView()
+        myImageView2.frame = CGRectMake(x, y, width, height)
+        myImageView2.image = UIImage(named: "photo-2.jpg")
+        myImageView2.contentMode = .ScaleAspectFill
+        myImageView2.clipsToBounds = true
+        myScrollView!.addSubview(myImageView2)
+        
+        // 3枚目の写真
+        x += width
+        let myImageView3 = UIImageView()
+        myImageView3.frame = CGRectMake(x, y, width, height)
+        myImageView3.image = UIImage(named: "photo-3.jpg")
+        myImageView3.contentMode = .ScaleAspectFill
+        myImageView3.clipsToBounds = true
+        myScrollView!.addSubview(myImageView3)
+        
+        // スクロールサイズ調整 (写真3枚スクロールするので、写真幅 x 3)
+        myScrollView!.contentSize.width = width * 3
+        
+        // ページングする
+        myScrollView!.pagingEnabled = true
     }
 
     override func didReceiveMemoryWarning() {
