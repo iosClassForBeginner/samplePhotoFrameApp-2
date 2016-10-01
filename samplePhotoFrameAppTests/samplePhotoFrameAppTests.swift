269cd23aae891f1d442887eca222267f3d64af3c
WataruMaeda <wm19850716@gmail.com>
2016-10-01 15:40:51 +0900

    Initial Commit

---
+++ samplePhotoFrameAppTests/samplePhotoFrameAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  samplePhotoFrameAppTests.swift
+//  samplePhotoFrameAppTests
+//
+//  Created by Wataru Maeda on 10/1/16.
+//  Copyright © 2016 wataru maeda. All rights reserved.
+//
+
+import XCTest
+@testable import samplePhotoFrameApp
+
+class samplePhotoFrameAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
